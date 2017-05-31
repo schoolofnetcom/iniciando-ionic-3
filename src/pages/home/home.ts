5ce150f6cfd10a726da4b92d4eafffa2ec7089f4
Erik Figueiredo <erik.figueiredo@gmail.com>
2017-05-31 14:04:23 -0300

    projeto

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Http, Response } from '@angular/http';
 import { NavController } from 'ionic-angular';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public pokes: any[];
 
+  constructor(public navCtrl: NavController, private http: Http) {
+
+  }
+
+  ngOnInit() {
+    this.http.get('http://pokeapi.co/api/v2/pokemon')
+        .map((res: Response) => {
+          let body = res.json();
+          return body || {};
+        })
+        .subscribe(pokes => this.pokes = pokes.results)
   }
 
 }
